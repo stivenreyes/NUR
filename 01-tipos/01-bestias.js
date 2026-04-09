4e08efd405a8464fa88145d5b4e572bb9848e8b9
stivenreyes <stivenreyesdesign@gmail.com>
2026-04-09 11:40:24 -0500

    Add files via upload

--- 01-tipos/01-bestias.js
+++ 01-tipos/01-bestias.js
@@ -182,7 +182,7 @@ function filtrarBestias() {
 let bestias = [
 {
   nombre: "Babosa Plateada",
-  bioma: "Bosques",
+  bioma: "Bosque",
   terreno: "Tierra",
   tipo: "Soporte",
   tamaño: "5",
@@ -199,7 +199,7 @@ let bestias = [
 
 { 
   nombre: "Gran Rana Purpura",
-  bioma: "Bosques",
+  bioma: "Bosque",
   terreno: "Tierra",
   tipo: "Ataque",
   tamaño: "5",
@@ -213,7 +213,7 @@ let bestias = [
 
 { 
   nombre: "Rata Carmesi",
-  bioma: "Bosques",
+  bioma: "Bosque",
   terreno: "Tierra",
   tipo: "Exploracion",
   tamaño: "3",
@@ -227,7 +227,7 @@ let bestias = [
 
 { 
   nombre: "Cuervo X2",
-  bioma: "Bosques",
+  bioma: "Bosque",
   terreno: "Aire",
   tipo: "Exploracion",
   tamaño: "3",
@@ -241,7 +241,7 @@ let bestias = [
 
 { 
   nombre: "Garza de las Lenguas",
-  bioma: "Costas",
+  bioma: "Costa",
   terreno: "Aire",
   tipo: "Comunicacion",
   tamaño: "5",
@@ -255,7 +255,7 @@ let bestias = [
 
 { 
   nombre: "Erizo de 4 Brazos",
-  bioma: "Costas",
+  bioma: "Costa",
   terreno: "Tierra",
   tipo: "Ataque",
   tamaño: "5",
@@ -269,7 +269,7 @@ let bestias = [
 
 { 
   nombre: "Tiburon Unicornio",
-  bioma: "Costas",
+  bioma: "Costa",
   terreno: "Agua",
   tipo: "Exploracion",
   tamaño: "10",
@@ -321,7 +321,243 @@ let bestias = [
   enfoques: "Aguante, Carga", 
   Habilidades: "Montura resistente a magia, transporte masivo, habilidad de rescate",
   ataque: "+1", daño: "1D4", dureza: "5", alcance: "1"
-}
+},
+{ 
+    nombre: "Mono albino",
+    bioma: "Bosque",
+    terreno: "Tierra",
+    tipo: "Ataque",
+    tamaño: "5",
+    armadura: "0",
+    Destreza: "3", Precision: "-1", pelea: "4", percepcion: "0", comunicacion: "0",
+    fuerza: "2", voluntad: "-1", constitucion: "1", Instinto: "-2",
+    enfoques: "Artes marciales", 
+    Habilidades: "Puede repetir un ataque fallido una vez usando su cola",
+    ataque: "+1", daño: "1D4", dureza: "3", alcance: "1"
+  },
+
+  { 
+    nombre: "Oso hormiguero verde",
+    bioma: "Bosque",
+    terreno: "Tierra",
+    tipo: "Rara",
+    tamaño: "5",
+    armadura: "10",
+    Destreza: "2", Precision: "4", pelea: "-1", percepcion: "3", comunicacion: "0",
+    fuerza: "0", voluntad: "1", constitucion: "-1", Instinto: "-2",
+    enfoques: "Oler", 
+    Habilidades: "Ataque oculto con lengua hasta 5m, canaliza poderes del amo, daño lengua 1D6",
+    ataque: "+1", daño: "1D4", dureza: "4", alcance: "1"
+  },
+
+  { 
+    nombre: "Rana dorada",
+    bioma: "Costa",
+    terreno: "Agua",
+    tipo: "Curandera",
+    tamaño: "5",
+    armadura: "0",
+    Destreza: "3", Precision: "0", pelea: "1", percepcion: "2", comunicacion: "1",
+    fuerza: "0", voluntad: "-1", constitucion: "-2", Instinto: "4",
+    enfoques: "Nadar, Curación, Huir", 
+    Habilidades: "Cura 2D6 (amo) o 1D8 (aliado), bonus cirugía +6, puede almacenar y curar aliados en estómago",
+    ataque: "+1", daño: "1D4", dureza: "0", alcance: "1"
+  },
+
+  { 
+    nombre: "Escarabajo amarillo",
+    bioma: "Costa",
+    terreno: "Tierra",
+    tipo: "Rara",
+    tamaño: "5",
+    armadura: "10",
+    Destreza: "0", Precision: "-1", pelea: "-1", percepcion: "1", comunicacion: "0",
+    fuerza: "2", voluntad: "4", constitucion: "3", Instinto: "-2",
+    enfoques: "", 
+    Habilidades: "Infecta con hongos al contacto, daño 2D6, una vez por día, tipo veneno",
+    ataque: "+1", daño: "1D4", dureza: "5", alcance: "1"
+  },
+
+  { 
+    nombre: "Pulpo cíclope",
+    bioma: "Costa",
+    terreno: "Agua",
+    tipo: "Estratégico",
+    tamaño: "3",
+    armadura: "0",
+    Destreza: "0", Precision: "0", pelea: "-1", percepcion: "2", comunicacion: "1",
+    fuerza: "1", voluntad: "3", constitucion: "-1", Instinto: "4",
+    enfoques: "Conocimiento espiritual, Nadar, Sumergirse", 
+    Habilidades: "Puede restaurar una poción una vez (pierde efecto original)",
+    ataque: "+1", daño: "1D4", dureza: "4", alcance: "1"
+  },
+
+  { 
+    nombre: "Cangrejo jaula",
+    bioma: "Costa",
+    terreno: "Tierra",
+    tipo: "Rara",
+    tamaño: "5",
+    armadura: "10",
+    Destreza: "0", Precision: "-1", pelea: "1", percepcion: "1", comunicacion: "0",
+    fuerza: "2", voluntad: "3", constitucion: "4", Instinto: "-2",
+    enfoques: "", 
+    Habilidades: "Almacena objetos en su caparazón y puede duplicarlo como salida de emergencia",
+    ataque: "+1", daño: "1D4", dureza: "6", alcance: "1"
+  },
+
+  { 
+    nombre: "Cangrejo espinoso",
+    bioma: "Costa",
+    terreno: "Tierra",
+    tipo: "Tanque",
+    tamaño: "15",
+    armadura: "20",
+    Destreza: "2", Precision: "0", pelea: "2", percepcion: "-1", comunicacion: "-1",
+    fuerza: "3", voluntad: "1", constitucion: "4", Instinto: "-2",
+    enfoques: "Aguante, Respiración, Sumergirse", 
+    Habilidades: "Sirve como montura, da oxígeno bajo el agua y transporta aliados según tamaño",
+    ataque: "+3", daño: "1D4", dureza: "6", alcance: "1"
+  },
+
+  { 
+    nombre: "Cacatúa morada",
+    bioma: "Costa",
+    terreno: "Viento",
+    tipo: "Artesana",
+    tamaño: "5",
+    armadura: "0",
+    Destreza: "3", Precision: "-1", pelea: "-1", percepcion: "2", comunicacion: "1",
+    fuerza: "0", voluntad: "0", constitucion: "-2", Instinto: "4",
+    enfoques: "Orientarse, Espiritual, Forrajear", 
+    Habilidades: "Puede crear y reparar objetos en plano físico y espiritual (+2 artesanía)",
+    ataque: "+1", daño: "1D4", dureza: "3", alcance: "1"
+  },
+
+  { 
+    nombre: "Medusa eléctrica",
+    bioma: "Costa",
+    terreno: "Agua",
+    tipo: "Ataque",
+    tamaño: "3",
+    armadura: "0",
+    Destreza: "-1", Precision: "-1", pelea: "0", percepcion: "2", comunicacion: "-1",
+    fuerza: "0", voluntad: "3", constitucion: "-2", Instinto: "4",
+    enfoques: "Nadar, Rayo", 
+    Habilidades: "Ataque eléctrico a 10m, daño 2D6 + Instinto, coste -2 nur",
+    ataque: "+1", daño: "1D4", dureza: "2", alcance: "1"
+  },
+{ 
+    nombre: "Lagarto de trueno",
+    bioma: "Desierto",
+    terreno: "Tierra",
+    tipo: "Ataque",
+    tamaño: "13",
+    armadura: "15",
+    Destreza: "4", Precision: "1", pelea: "2", percepcion: "-1", comunicacion: "-2",
+    fuerza: "1", voluntad: "1", constitucion: "0", Instinto: "3",
+    enfoques: "Iniciativa, Velocidad", 
+    Habilidades: "Puede lanzar un rayo 1D6 (dif 10, -3 nur) sin acción una vez por turno. Funciona como montura y transporte según tamaño",
+    ataque: "+3", daño: "1D4", dureza: "4", alcance: "1"
+  },
+
+  { 
+    nombre: "Boa espinosa",
+    bioma: "Desierto",
+    terreno: "Tierra",
+    tipo: "Rara",
+    tamaño: "5",
+    armadura: "0",
+    Destreza: "-1", Precision: "0", pelea: "-1", percepcion: "1", comunicacion: "0",
+    fuerza: "2", voluntad: "3", constitucion: "4", Instinto: "-2",
+    enfoques: "", 
+    Habilidades: "Aprisiona enemigos y los paraliza (3D6 min). Dificultad basada en fuerza (+6 menor rango, +4 igual, +3 mayor)",
+    ataque: "+1", daño: "1D4", dureza: "4", alcance: "1"
+  },
+
+  { 
+    nombre: "Camaleón triclope",
+    bioma: "Desierto",
+    terreno: "Tierra",
+    tipo: "Sigilo",
+    tamaño: "3",
+    armadura: "5",
+    Destreza: "4", Precision: "0", pelea: "-1", percepcion: "2", comunicacion: "1",
+    fuerza: "0", voluntad: "1", constitucion: "-1", Instinto: "3",
+    enfoques: "Sigilo, Juego de manos", 
+    Habilidades: "Invisible 1D6 turnos (dif 20 detectarlo). Puede hacerse invisible con su amo (1 vez/día)",
+    ataque: "+1", daño: "1D4", dureza: "2", alcance: "1"
+  },
+
+  { 
+    nombre: "Cóndor tricolor",
+    bioma: "Desierto",
+    terreno: "Viento",
+    tipo: "Montura",
+    tamaño: "14",
+    armadura: "5",
+    Destreza: "4", Precision: "0", pelea: "-1", percepcion: "3", comunicacion: "1",
+    fuerza: "1", voluntad: "2", constitucion: "0", Instinto: "-1",
+    enfoques: "Volar, Huir, Olor", 
+    Habilidades: "Montura aérea. Evita daño por caídas y es inmune temporal a terrenos peligrosos",
+    ataque: "+1", daño: "1D4", dureza: "3", alcance: "1"
+  },
+
+  { 
+    nombre: "Rana blanca",
+    bioma: "Montaña",
+    terreno: "Tierra",
+    tipo: "Clima",
+    tamaño: "3",
+    armadura: "0",
+    Destreza: "1", Precision: "0", pelea: "-2", percepcion: "4", comunicacion: "-1",
+    fuerza: "1", voluntad: "2", constitucion: "3", Instinto: "3",
+    enfoques: "Rastrear, Clima, Nadar", 
+    Habilidades: "Detecta clima futuro (5h). Hace inmune a clima por 1D6 turnos (1 vez/día)",
+    ataque: "-1", daño: "1D4", dureza: "2", alcance: "1"
+  },
+
+  { 
+    nombre: "Topo de 4 brazos",
+    bioma: "Montaña",
+    terreno: "Tierra",
+    tipo: "Rara",
+    tamaño: "3",
+    armadura: "0",
+    Destreza: "-1", Precision: "0", pelea: "-1", percepcion: "1", comunicacion: "0",
+    fuerza: "2", voluntad: "3", constitucion: "4", Instinto: "-2",
+    enfoques: "Excavar, Huir", 
+    Habilidades: "Viaja bajo tierra, ataques sorpresa: +4D6 (desprevenido), +2D6 (alerta), +1D6 (guardia)",
+    ataque: "+1", daño: "1D4", dureza: "5", alcance: "1"
+  },
+
+  { 
+    nombre: "Salamandra de dos colas",
+    bioma: "Montaña",
+    terreno: "Tierra",
+    tipo: "Rara",
+    tamaño: "5",
+    armadura: "0",
+    Destreza: "0", Precision: "-1", pelea: "2", percepcion: "1", comunicacion: "0",
+    fuerza: "0", voluntad: "4", constitucion: "2", Instinto: "-2",
+    enfoques: "Lanzar", 
+    Habilidades: "Escupe veneno: 4D6 inicial + 1D6 continuo (1 vez por combate)",
+    ataque: "+1", daño: "1D4", dureza: "2", alcance: "1"
+  },
+
+  { 
+    nombre: "Lechuza de 4 alas",
+    bioma: "Montaña",
+    terreno: "Viento",
+    tipo: "Exploración",
+    tamaño: "5",
+    armadura: "0",
+    Destreza: "2", Precision: "-1", pelea: "0", percepcion: "1", comunicacion: "2",
+    fuerza: "0", voluntad: "-1", constitucion: "0", Instinto: "3",
+    enfoques: "Sigilo, Volar", 
+    Habilidades: "De noche se vuelve invisible (1D3 horas). Detectarla requiere tirada enfrentada alta",
+    ataque: "+1", daño: "1D4", dureza: "3", alcance: "1"
+  }  
 
 ]
 
